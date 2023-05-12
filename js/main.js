b6e9e9a27dda383307100de5fe3f1a2912511858
Sabrina Drashen <sdrashen@gmail.com>
2023-05-12 00:11:06 -0300

    uses forEach

--- js/main.js
+++ js/main.js
@@ -3,16 +3,18 @@ const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
 const controle = document.querySelectorAll(".controle-ajuste");
-console.log(controle);
 
-somar.addEventListener("click", () => {manipulaDados("somar")})
-
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent);
+    })
+    
+})
 
 function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
-    braco.value = parseInt(braco.value) - 1;
+    if(operacao === "-") {
+        braco.value = parseInt(braco.value) - 1;
     } else {
-    braco.value = parseInt(braco.value) + 1;
+        braco.value = parseInt(braco.value) + 1;
     }
 }
